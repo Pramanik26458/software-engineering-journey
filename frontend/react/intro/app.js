bb187b2c1ef7feeab74f19db4a55c2623111bd55
Pramanik26458 <basakpramanik9@gmail.com>
2026-02-25 15:55:46 +0530

    Started React journey with multi-file component structure

---
+++ frontend/react/intro/app.js
@@ -0,0 +1,4 @@
+const box=()=>{
+    return React.createElement('div',{id:'box'},'this is box')
+}
+export default box
---
+++ frontend/react/intro/test.js
@@ -0,0 +1,4 @@
+const circle =()=>{
+    return React.createElement('div',{id:"circle"},"this is circle")
+}
+export default circle
