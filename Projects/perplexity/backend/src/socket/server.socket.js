c087977663526ec6cd3bfd7d658422e7d04faf97
Pramanik26458 <basakpramanik9@gmail.com>
2026-06-25 15:27:22 +0530

    Fix Socket.IO production CORS

--- Projects/perplexity/backend/src/socket/server.socket.js
+++ Projects/perplexity/backend/src/socket/server.socket.js
@@ -1,25 +1,34 @@
-import {Server} from "socket.io"
+import { Server } from "socket.io";
 
 let io;
 
-export function initSocket(httpServer){
-    io=new Server(httpServer,{
-        cors:{
-            origin:"http://localhost:5173",
-            credentials:true,
-        }
-    })
+export function initSocket(httpServer) {
+  io = new Server(httpServer, {
+    cors: {
+      origin: [
+        "http://localhost:5173",
+        process.env.FRONTEND_URL,
+      ],
+      credentials: true,
+      methods: ["GET", "POST"],
+    },
+  });
 
-    console.log("socket io server is running")
+  console.log("✓ Socket.IO server is running");
 
-    io.on("connection",(socket)=>{
-                console.log("A user conntected: "+socket.id)
-})
+  io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
+  });
 }
 
-export function getIO(){
-    if(!io){
-        throw new Error("Socket.io not initilized");
-    }
-    return io;
+export function getIO() {
+  if (!io) {
+    throw new Error("Socket.io not initialized");
+  }
+
+  return io;
 }
