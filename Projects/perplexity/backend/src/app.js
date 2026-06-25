ec4590952e5914aadfc537975b346a5180d50364
Pramanik26458 <basakpramanik9@gmail.com>
2026-06-25 16:47:05 +0530

    fixing the cors problem

--- Projects/perplexity/backend/src/app.js
+++ Projects/perplexity/backend/src/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors({
-  origin: process.env.FRONTEND_URL, 
+  origin: "*", 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
